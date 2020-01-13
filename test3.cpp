6b4978def4d456961dcbb850526f503977e87fa1
root <stronck@chapman.edu>
2020-01-13 23:36:41 +0000

    Now I'm editting the readme

--- test3.cpp
+++ test3.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
+using namespace std;
 
 int main (int argc, char **argv){
-  cout << "hi";
+  cout << "hi" << endl;
   cout << "I had to make this statement because Rene said so" << endl;
   return 0;
 }
