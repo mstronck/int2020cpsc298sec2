679f274ed1162c4238895d493b594b06f1c3a37e
root <stronck@chapman.edu>
2020-01-14 00:28:57 +0000

    argc argv stuff

---
+++ Assignment1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+int main (int argc, char **argv){
+  cout << "yuh";
+}
--- test3.cpp
+++ test3.cpp
@@ -4,6 +4,10 @@
 using namespace std;
 
 int main (int argc, char **argv){
+  cout << "Command line arg count: " << argc << endl;
+
+  cout << "My name is " << argv[2] << endl;
+
   cout << "hi" << endl;
   cout << "I had to make this statement because Rene said so" << endl;
   return 0;
