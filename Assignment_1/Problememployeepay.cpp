356b3a508a6aa47a1cd5b01f62386987ef910e3b
root <stronck@chapman.edu>
2020-01-14 17:34:19 +0000

    completed Problememployeepay

--- Assignment_1/Problememployeepay.cpp
+++ Assignment_1/Problememployeepay.cpp
@@ -20,11 +20,42 @@ int main (int argc, char **argv){
   //initializing variables
   const float pay_rate = 16.00;
   const float ot_pay_rate = pay_rate * 1.5;
-  const float ss_tax = .06;
-  const float fed_tax = .14;
-  const float state_tax = .05;
+  const float ss_tax_rate = .06;
+  const float fed_tax_rate = .14;
+  const float state_tax_rate = .05;
   const int med_ins = 10; //$ dollars
+  float hours_worked = 0.0;
+  float ot_hours_worked = 0.0;
+  float netincome = 0.0;
 
+  //Recieving user input
+  cout << "Input the number of normal hours worked in a week." << endl;
+  cin >> hours_worked;
+  cout << "Input the number of over time horus worked in a week." << endl;
+  cin >> ot_hours_worked;
+
+  //Calculating take-home pay
+  netincome = (hours_worked * pay_rate) + (ot_hours_worked * ot_pay_rate);
+  // Social Security
+  float ss_tax = netincome * ss_tax_rate;
+  netincome = netincome - (netincome * ss_tax_rate);
+  //Federal income tax
+  float fed_tax = netincome * fed_tax_rate;
+  netincome = netincome - (netincome * fed_tax_rate);
+  //state income tax
+  float state_tax = netincome * state_tax_rate;
+  netincome = netincome - (netincome * state_tax_rate);
+  //medical insurance
+  netincome = netincome - med_ins;
+
+  //Outputting to user
+  cout << "Net income: " << (hours_worked * pay_rate) + (ot_hours_worked * ot_pay_rate)<< endl;
+  cout << "Social Security tax: " << ss_tax << endl;
+  cout << "Federal income tax: " << fed_tax << endl;
+  cout << "State income tax: " << state_tax << endl;
+  cout << "Medical Insurance Expense: " << med_ins << endl;
+  cout << "----------------------------------------" << endl;
+  cout << "Take-home pay: " << netincome << endl;
 
   return 0;
 }
